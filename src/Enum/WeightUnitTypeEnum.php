5afd9cec6839b1629ed281dedfa444f75e04b613
Mariano Barros <marianobarros@protonmail.com>
2025-02-08 19:03:24 +0100

    remove duplicated enum

--- src/Enum/WeightUnitTypeEnum.php
+++ src/Enum/WeightUnitTypeEnum.php
@@ -4,7 +4,23 @@ namespace App\Enum;
 
 enum WeightUnitTypeEnum: string
 {
-    case KG = 'kg';
-    case G = 'g';
+    case KILOGRAMS = 'kg';
+    case GRAMS = 'g';
+
+    public function toGrams(): int
+    {
+        return match ($this) {
+            self::KILOGRAMS => 1000,
+            self::GRAMS => 1,
+        };
+    }
+
+    public function toKilograms(): float
+    {
+        return match ($this) {
+            self::KILOGRAMS => 1,
+            self::GRAMS => 0.001,
+        };
+    }
 }
 
